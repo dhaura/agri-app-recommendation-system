b33f8027886d4b444626ce5b1c71f31ba7758246
Dhaura Pathirana <dhaurapathirana@gmail.com>
2023-01-05 10:54:04 +0530

    update ml model

--- CNN.py
+++ CNN.py
@@ -6,61 +6,35 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv_layers = nn.Sequential(
             # conv1
-            nn.Conv2d(in_channels=3, out_channels=32,
-                      kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(32),
-            nn.Conv2d(in_channels=32, out_channels=32,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
+            nn.AvgPool2d(8),
             # conv2
-            nn.Conv2d(in_channels=32, out_channels=64,
-                      kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.BatchNorm2d(64),
-            nn.Conv2d(in_channels=64, out_channels=64,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            # conv3
-            nn.Conv2d(in_channels=64, out_channels=128,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(in_channels=128, out_channels=128,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(2),
-            # conv4
-            nn.Conv2d(in_channels=128, out_channels=256,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(256),
-            nn.Conv2d(in_channels=256, out_channels=256,
-                      kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(2),
+            nn.AvgPool2d(4)
         )
 
         self.dense_layers = nn.Sequential(
             nn.Dropout(0.4),
-            nn.Linear(50176, 1024),
+            nn.Linear(3136, 512),
             nn.ReLU(),
             nn.Dropout(0.4),
-            nn.Linear(1024, K),
+            nn.Linear(512, K),
         )
 
     def forward(self, X):
         out = self.conv_layers(X)
-
         # Flatten
-        out = out.view(-1, 50176)
-
+        out = out.flatten(start_dim=1)
         # Fully connected
         out = self.dense_layers(out)
 
